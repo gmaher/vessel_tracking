e48830f86dcb5183b714c91ce959612bef6a403e
marsdenlab <gdmaher@stanford.edu>
2018-08-14 15:48:31 -0700

    2d test case

---
+++ test/test_cases/branch2d/generate.py
@@ -0,0 +1,55 @@
+import numpy as np
+import os
+import sys
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.abspath('../../..'))
+
+from vessel_tracking import util
+
+def vessel_func(i,j,I):
+    return np.mean(I[i-1:i+1,j-1:j+1])
+
+N = 50
+NOISE_FACTOR = 0.35
+
+Noise = np.random.randn(N,N)*NOISE_FACTOR
+
+counts = np.zeros((N,N))
+
+I = np.zeros((N,N))
+
+x = np.linspace(-1,1,N)
+X,Y = np.meshgrid(x,x)
+
+#create image
+I[N//2, 0:N//3]   = 1.0
+I[N//2+1, 0:N//3] = 1.0
+I[N//2+2, 0:N//3] = 1.0
+I[N//2+3, 0:N//3] = 1.0
+
+
+I[N//10:N//2, N//5-1] = 1.0
+I[N//10:N//2, N//5-2] = 1.0
+
+I[N//3, N//5-1:N//2] = 1.0
+
+I[N//3-2, N//5-1:int(N*3/4)] = 1.0
+
+I[N//3:N//2, N//2-5] = 1.0
+
+I[N//2:int(N*0.9), N//5-5] = 1.0
+I[N//2:int(N*0.9), N//5-6] = 1.0
+
+I[int(N*0.75), N//5-5:int(0.9*N)] = 1.0
+
+plt.figure()
+plt.imshow(I,cmap='gray')
+plt.colorbar()
+plt.show()
+
+plt.figure()
+plt.imshow(I+Noise,cmap='gray')
+plt.colorbar()
+plt.show()
