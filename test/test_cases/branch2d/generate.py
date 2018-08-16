a78d0648e0c7b45032049413044e6d2cc7ee54de
marsdenlab <gdmaher@stanford.edu>
2018-08-16 09:40:15 -0700

    exploration

--- test/test_cases/branch2d/generate.py
+++ test/test_cases/branch2d/generate.py
@@ -11,6 +11,16 @@ from vessel_tracking import util
 def vessel_func(i,j,I):
     return np.mean(I[i-1:i+1,j-1:j+1])
 
+def action_to_move(a):
+    if a == 0:
+        return (0,-1)
+    if a == 1:
+        return (1,0)
+    if a == 2:
+        return (0,1)
+    if a == 3:
+        return (-1,0)
+
 N = 50
 NOISE_FACTOR = 0.35
 
@@ -47,11 +57,12 @@ I[int(N*0.75), N//5-5:int(0.9*N)] = 1.0
 ###################
 # Sim
 ###################
-Nsim   = 100
-Nsteps = 10
-eps = 0.2
+Nsim   = 1000
+Nsteps = 5000
+eps = 0.75
+gamma = 0.9
 
-x_start = 0
+x_start = 1
 y_start = N//2
 
 V = np.zeros((N,N))
@@ -59,16 +70,38 @@ V = np.zeros((N,N))
 for i in range(Nsim):
     x = x_start
     y = y_start
-    for i in range(Nsteps):
+
+    print ("Sim {}".format(i))
+
+    for j in range(Nsteps):
+
+        print("step {}, y={} x={}".format(j,y,x))
 
         r = np.random.rand()
 
         if r < eps:
-            a = np.randint(4)
+            a = np.random.randint(4)
 
         else:
-            
+            a = np.argmax([ V[y,x-1], V[y+1,x], V[y,x+1], V[y-1,x] ])
 
+        print("action {}".format(a))
+        mov = action_to_move(a)
+
+        xx = x+mov[0]
+        yy = y+mov[1]
+
+        if xx < 1:   xx = 1
+        if xx > N-2: xx = N-2
+        if yy < 1:   yy = 1
+        if yy > N-2: yy = N-2
+
+        r = vessel_func(yy,xx,I)
+
+        V[y,x] = r + gamma*V[yy,xx]
+
+        x = xx
+        y = yy
 ###################
 # Plots
 ###################
@@ -79,5 +112,10 @@ plt.show()
 
 plt.figure()
 plt.imshow(I+Noise,cmap='gray')
+plt.colorbar()
+plt.show()
+
+plt.figure()
+plt.imshow(V,cmap='gray')
 plt.colorbar()
 plt.show()
