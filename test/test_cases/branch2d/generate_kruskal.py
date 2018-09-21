ca9bb2f5c7ce21146d75cfd6c9511c3860da9367
gabriel <gmaher2@hotmail.com>
2018-09-21 14:59:13 -0700

    kruskal

---
+++ test/test_cases/branch2d/generate_kruskal.py
@@ -0,0 +1,170 @@
+import numpy as np
+import os
+import sys
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+from queue import Queue
+
+sys.path.append(os.path.abspath('../../..'))
+
+from vessel_tracking import util, rl
+
+class CollisionDetector(object):
+    def __init__(self,I_int,n=100):
+        self.I_int = I_int
+        self.n = n
+
+    def collision(self,p1,p2):
+        for i in np.linspace(0,1,self.n):
+            p = (1-i)*p1+i*p2
+            if float(self.I_int(p)) <= 0.1:
+                return True
+        return False
+
+def vessel_func(i,j,I):
+    return np.mean(I[i-1:i+1,j-1:j+1])
+
+def action_to_move(a):
+    if a == 0:
+        return (0,-1)
+    if a == 1:
+        return (1,0)
+    if a == 2:
+        return (0,1)
+    if a == 3:
+        return (-1,0)
+
+N = 50
+NOISE_FACTOR = 0.35
+
+Noise = np.random.randn(N,N)*NOISE_FACTOR
+
+counts = np.zeros((N,N))
+
+I = np.zeros((N,N))
+
+x = np.linspace(-1,1,N)
+X,Y = np.meshgrid(x,x)
+
+#create image
+I[N//2, 0:N//3]   = 1.0
+I[N//2+1, 0:N//3] = 1.0
+I[N//2+2, 0:N//3] = 1.0
+I[N//2+3, 0:N//3] = 1.0
+
+
+I[N//10:N//2, N//5-1] = 1.0
+I[N//10:N//2, N//5-2] = 1.0
+
+I[N//3, N//5-1:N//2] = 1.0
+
+I[N//3-2, N//5-1:int(N*3/4)] = 1.0
+
+I[N//3:N//2, N//2-5] = 1.0
+
+I[N//2:int(N*0.9), N//5-5] = 1.0
+I[N//2:int(N*0.9), N//5-6] = 1.0
+
+I[int(N*0.75), N//5-5:int(0.9*N)] = 1.0
+
+I_vec = np.zeros((N*N))
+points = np.zeros((N*N,2))
+for i in range(N):
+    for j in range(N):
+        points[j+i*N,0] = i
+        points[j+i*N,1] = j
+
+        I_vec[j+i*N] = I[i,j]
+
+
+from scipy.interpolate import LinearNDInterpolator
+
+Im_int = LinearNDInterpolator(points+0.5, np.ravel(I))
+
+Nsamp = 2000
+p_samp = np.random.rand(Nsamp,2)*N
+vals = Im_int(p_samp)
+
+V_free = p_samp[vals>0]
+###################
+# PRM
+###################
+p_start = np.array([26,1])
+
+V_free = np.concatenate((p_start[np.newaxis,:],V_free),axis=0)
+Nfree = V_free.shape[0]
+
+q = Queue()
+K = 10
+DIST_CUTOFF=100
+
+connected=False
+
+in_tree = np.zeros((V_free.shape[0]))
+in_tree[0] = 1
+
+CD = CollisionDetector(Im_int)
+
+edges = []
+for i in range(Nfree):
+    #for each node get K nearest neighbors that are collison free
+    p = V_free[i]
+    dists = np.sum((V_free-p)**2,axis=1)
+
+    dists[i] = 1e10
+
+    idx   = np.argpartition(dists,K)[:K]
+
+    for j in range(K):
+        p_next = V_free[idx[j]]
+        if not (p_next[0]==p[0] and p_next[1]==p[1]):
+            if not CD.collision(p,p_next):
+                c = np.sum((p-p_next)**2)
+                t = ((i,idx[j]), (p,p_next), c)
+                edges.append(t)
+
+edges_sorted = sorted(edges, key=lambda t: t[2])
+tree_edges = []
+neighbor_sets = []
+visited_nodes = {}
+
+for i in range(Nfree):
+    s = set()
+    s.add(i)
+    neighbor_sets.append(s)
+
+for e in edges_sorted:
+    u_id = e[0][0]
+    v_id = e[0][1]
+    if not neighbor_sets[u_id] == neighbor_sets[v_id]:
+        tree_edges.append(e)
+        a = neighbor_sets[u_id].union(neighbor_sets[v_id])
+        neighbor_sets[u_id] = a
+        neighbor_sets[v_id] = a
+
+###################
+# Plots
+###################
+plt.figure()
+plt.imshow(I,cmap='gray')
+plt.colorbar()
+plt.show()
+
+
+plt.figure()
+plt.imshow(I, extent=[0, N, N, 0], cmap='gray')
+plt.colorbar()
+
+for i in range(Nsamp):
+    if vals[i] > 0:
+        color = 'b'
+
+        plt.scatter(p_samp[i,1], p_samp[i,0], color=color)
+
+for t in edges:
+    p1 = t[1][0]
+
+    p2 = t[1][1]
+
+    plt.plot([p1[1],p2[1]], [p1[0],p2[0]], color='r')
+plt.show()
